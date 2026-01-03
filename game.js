02dcec3fa0404dccc4a03b6ee3f784417d32d373
Gab_Destal <g.destal007@gmail.com>
2026-01-03 15:09:39 +0100

    Add event listeners for play and back buttons

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
+const menu = document.getElementById("menu");
+const game = document.getElementById("game");
+const playBtn = document.getElementById("playBtn");
+const backBtn = document.getElementById("backBtn");
+
+playBtn.addEventListener("click", () => {
+  menu.classList.add("hidden");
+  game.classList.remove("hidden");
+});
+
+backBtn.addEventListener("click", () => {
+  game.classList.add("hidden");
+  menu.classList.remove("hidden");
+});
 
